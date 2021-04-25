76e3879388b10512d1a5c77eddbe6aa207271b1c
park <kunwook9826@hanyang.ac.kr>
2021-04-26 02:45:08 +0900

    bubblesort

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+void bubble(int* arr, int n){
+	int tmp;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-1; j++){
+			if(arr[j] > arr[j+1]){
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+	int tmp;
+	int *arr = new int[n];
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
+	}
+	bubble(arr, n);
+	for(int i=0; i<n; i++){
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}	
+
